f118c5e5372e4050fe7c3fcce94238041a9cfe4d
Nick Lee <nicklee808@gmail.com>
2017-02-13 20:55:07 -1000

    routes setup

--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const db = require('./../models');
+const {Photo} = db;
 
 router.route('/:id')
   .get((req, res) => {
@@ -8,6 +10,22 @@ router.route('/:id')
   })
   .put((req, res) => {
 
+  })
+  .post((req, res) => {
+    res.redirect();
+  })
+  .delete((req, res) => {
+    res.redirect();
+  });
+
+router.route('/new')
+  .get((req, res) => {
+    res.render('');
+  });
+
+router.route('/:id/edit')
+  .get((req, res) => {
+    res.render('');
   });
 
 module.exports = router;
